69906fc1bfb0afc012955e36b363cef189302ff0
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2023-02-03 10:18:54 +0100

    added all css style and added style.display to js

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,15 @@
 const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
+const nav = document.getElementById('nav-bar')
 
-open.addEventListener( 'click', () => container.classList.add('show-nav'));
-close.addEventListener( 'click', () => container.classList.remove('show-nav'));
+open.addEventListener( 'click', () =>{
+  container.classList.add('show-nav');
+  nav.style.display = "block";
+});
+
+close.addEventListener( 'click', () => {
+   container.classList.remove('show-nav');
+   nav.style.display = "none";
+});
+
